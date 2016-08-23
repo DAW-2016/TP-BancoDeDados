a00a94e4f4bf62dee1ce2b17dcccff111ea0093e
asantos07 <as-ariel@outlook.com>
2016-08-23 12:45:45 -0300

    Script de criação MySQL

---
+++ script.sql
@@ -0,0 +1,99 @@
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Table `Tipo`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Tipo` (
+  `nome` VARCHAR(15) NOT NULL,
+  PRIMARY KEY (`nome`))
+ENGINE = InnoDB;
+
+CREATE UNIQUE INDEX `nome_UN` ON `Tipo` (`nome` ASC);
+
+
+-- -----------------------------------------------------
+-- Table `Pokemon`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Pokemon` (
+  `id` INT NOT NULL,
+  `tipo` VARCHAR(15) NOT NULL,
+  `locais` INT NULL,
+  `nome` VARCHAR(25) NOT NULL,
+  `atk` INT NULL,
+  `def` INT NULL,
+  `peso` DECIMAL(4,2) NULL,
+  PRIMARY KEY (`id`, `nome`),
+  CONSTRAINT `fk_Pokemon_Tipo`
+    FOREIGN KEY (`tipo`)
+    REFERENCES `Tipo` (`nome`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE UNIQUE INDEX `nome_UNIQUE` ON `Pokemon` (`nome` ASC);
+
+CREATE INDEX `fk_Pokemon_Tipo_idx` ON `Pokemon` (`tipo` ASC);
+
+
+-- -----------------------------------------------------
+-- Table `Localizacao`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Localizacao` (
+  `id` INT NOT NULL,
+  `local` TEXT NULL,
+  `lat` DECIMAL(2,2) NOT NULL,
+  `lon` DECIMAL(2,2) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `PokemonLocalizacao`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `PokemonLocalizacao` (
+  `Pokemon_id` INT NOT NULL,
+  `Pokemon_nome` VARCHAR(25) NOT NULL,
+  `Localizacao_id` INT NOT NULL,
+  PRIMARY KEY (`Pokemon_id`, `Pokemon_nome`, `Localizacao_id`),
+  CONSTRAINT `fk_PokemonLocalizacao_Pokemon`
+    FOREIGN KEY (`Pokemon_id` , `Pokemon_nome`)
+    REFERENCES `Pokemon` (`id` , `nome`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_PokemonLocalizacao_Localizacao`
+    FOREIGN KEY (`Localizacao_id`)
+    REFERENCES `Localizacao` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `PokemonLocalizacao_Localizacao` ON `PokemonLocalizacao` (`Localizacao_id` ASC);
+
+CREATE INDEX `PokemonLocalizacao_Pokemon` ON `PokemonLocalizacao` (`Pokemon_id` ASC, `Pokemon_nome` ASC);
+
+
+-- -----------------------------------------------------
+-- Table `Evolucao`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Evolucao` (
+  `id` INT NOT NULL,
+  `pokemon` INT NOT NULL,
+  `evolucao` INT NULL,
+  PRIMARY KEY (`id`, `pokemon`),
+  CONSTRAINT `fk_Evolucao_Pokemon_id`
+    FOREIGN KEY (`pokemon`)
+    REFERENCES `Pokemon` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Pokemon_Pokemon_id`
+    FOREIGN KEY (`evolucao`)
+    REFERENCES `Pokemon` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `Evolucao_Pokemon_id` ON `Evolucao` (`pokemon` ASC);
+
+CREATE UNIQUE INDEX `evolucao_UN` ON `Evolucao` (`evolucao` ASC);
